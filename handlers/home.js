c51f62dbe082a5845e085eba3d1f3628bac2cde8
unknown <tsvetanmanev@gmail.com>
2017-06-06 17:02:21 +0300

    Included extra buttons under all products in index page

--- handlers/home.js
+++ handlers/home.js
@@ -1,17 +1,16 @@
-const fs = require('fs')
-const path = require('path')
-
 const Product = require('../models/Product')
 
 module.exports.index = (req, res) => {
   let queryData = req.query
 
-  Product.find().then((products) => {
-    if (queryData.query) {
-      products = products.filter(
-        p => p.name.toLowerCase().includes(queryData.query))
-    }
+  Product.find()
+    .populate('category')
+    .then((products) => {
+      if (queryData.query) {
+        products = products.filter(
+          p => p.name.toLowerCase().includes(queryData.query))
+      }
 
-    res.render('home/index', { products: products })
-  })
+      res.render('home/index', { products: products })
+    })
 }
