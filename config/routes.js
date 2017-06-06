2f4129e51fc984e546b916dfd54c4d816058fe4f
unknown <tsvetanmanev@gmail.com>
2017-06-06 22:48:35 +0300

    Implemented /category/:category/products. Fixed Add Product multer bugs.

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,16 @@
 const handlers = require('../handlers')
 const multer = require('multer')
 
+let upload = multer({dest: './content/images'})
+
 module.exports = (app) => {
   app.get('/', handlers.home.index)
 
   app.get('/product/add', handlers.product.addGet)
-  // app.post('/product/add', multer.upload.single('image'), handlers.product.addPost)
+  app.post('/product/add', upload.single('image'), handlers.product.addPost)
 
   app.get('/category/add', handlers.category.addGet)
   app.post('/category/add', handlers.category.addPost)
+
+  app.get('/category/:category/products', handlers.category.productByCategory)
 }
--- handlers/product.js
+++ handlers/product.js
@@ -1,36 +1,14 @@
-const path = require('path')
-const fs = require('fs')
-
 const Product = require('../models/Product')
 const Category = require('../models/Category')
 
 module.exports.addGet = (req, res) => {
-  let filePath = path.normalize(path.join(__dirname, '../views/product/add.html'))
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      console.log(err)
-      return
-    }
-
-    Category.find().then((categories) => {
-      let replacement = '<select class="input-field" name="category">'
-      for (let category of categories) {
-        replacement += `$<option value="${category._id}">${category.name}</option>`
-      }
-      replacement += `</select>`
-
-      let html = data.toString().replace('{categories}', replacement)
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
-      })
-      res.write(html)
-      res.end()
+  Category.find()
+    .then((categories) => {
+      res.render('product/add', {categories: categories})
     })
-  })
 }
 
-module.exports.addPost = (req, res) => {
+module.exports.addPost = (req, res, next) => {
   let productObj = req.body
   productObj.image = '\\' + req.file.path
 
