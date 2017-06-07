53bd255d62be8c7342d5330f517a8f7ed983cb6b
unknown <tsvetanmanev@gmail.com>
2017-06-07 13:52:26 +0300

    Implemented editing functionality

--- config/routes.js
+++ config/routes.js
@@ -9,6 +9,9 @@ module.exports = (app) => {
   app.get('/product/add', handlers.product.addGet)
   app.post('/product/add', upload.single('image'), handlers.product.addPost)
 
+  app.get('/product/edit/:id', handlers.product.editGet)
+  app.post('/product/edit/:id', upload.single('image'), handlers.product.editPost)
+
   app.get('/category/add', handlers.category.addGet)
   app.post('/category/add', handlers.category.addPost)
 
--- handlers/product.js
+++ handlers/product.js
@@ -20,3 +20,64 @@ module.exports.addPost = (req, res, next) => {
     res.redirect('/')
   })
 }
+
+module.exports.editGet = (req, res) => {
+  let id = req.params.id
+  Product.findById(id).then(product => {
+    if (!product) {
+      res.sendStatus(404)
+      return
+    }
+
+    Category.find().then((categories) => {
+      res.render('product/edit', {
+        product: product,
+        categories: categories
+      })
+    })
+  })
+}
+
+module.exports.editPost = (req, res) => {
+  let id = req.params.id
+  let editedProduct = req.body
+
+  Product.findById(id).then((product) => {
+    if (!product) {
+      res.redirect(`/?error=${encodeURIComponent('error=Product was not found!')}`)
+    }
+
+    product.name = editedProduct.name
+    product.description = editedProduct.description
+    product.price = editedProduct.price
+
+    if (req.file) {
+      product.image = '\\' + req.file.path
+    }
+
+    if (product.category.toString() !== editedProduct.category) {
+      Category.findById(product.category).then((currentCategory) => {
+        Category.findById(editedProduct.category).then((nextCategory) => {
+          let index = currentCategory.products.indexOf(product._id)
+          if (index >= 0) {
+            currentCategory.products.splice(index, 1)
+          }
+          currentCategory.save()
+
+          nextCategory.products.push(product._id)
+          nextCategory.save()
+
+          product.category = editedProduct.category
+
+          product.save().then(() => {
+            res.redirect(`/?success=${encodeURIComponent('Product was edited successfully')}`)
+          })
+        })
+      })
+    } else {
+      product.save().then(() => {
+        res.redirect(`/?success=${encodeURIComponent('Product was edited successfully!')}`)
+      })
+    }
+  })
+}
